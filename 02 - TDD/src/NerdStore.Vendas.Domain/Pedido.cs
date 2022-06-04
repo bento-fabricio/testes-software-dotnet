a31b7e050aa9b49a521ef89ad9e4e256aad544b6
Fabrício Bento <fabricio.bento@yahoo.com.br>
2022-06-04 10:00:38 -0300

    Quantidade mínima e máxima de itens de produto.

--- 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NerdStore.Core.DomainObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +7,9 @@ namespace NerdStore.Vendas.Domain
 {
     public class Pedido
     {
+        public static int MAX_UNIDADES_ITEM => 15;
+        public static int MIN_UNIDADES_ITEM => 1;
+
         protected Pedido()
         {
             _pedidoItems = new List<PedidoItem>();
@@ -20,13 +24,15 @@ namespace NerdStore.Vendas.Domain
         private readonly List<PedidoItem> _pedidoItems;
         public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
 
-        public void CalcularValorPedido()
+        private void CalcularValorPedido()
         {
             ValorTotal = PedidoItems.Sum(i => i.CalcularValor());
         }
 
         public void AdicionarItem(PedidoItem pedidoItem)
         {
+            if (pedidoItem.Quantidade > MAX_UNIDADES_ITEM) throw new DomainException($"Máximo de {MAX_UNIDADES_ITEM} unidades por produto.");
+
             if (_pedidoItems.Any(p => p.ProdutoId == pedidoItem.ProdutoId))
             {
                 var itemExistente = _pedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId);
@@ -80,6 +86,8 @@ namespace NerdStore.Vendas.Domain
 
         public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
         {
+            if (quantidade < Pedido.MIN_UNIDADES_ITEM) throw new DomainException($"Mínimo de {Pedido.MIN_UNIDADES_ITEM} unidades por produto.");
+
             ProdutoId = produtoId;
             ProdutoNome = produtoNome;
             Quantidade = quantidade;
@@ -96,4 +104,6 @@ namespace NerdStore.Vendas.Domain
             return Quantidade * ValorUnitario;
         }
     }
+
+    
 }
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -0,0 +1,19 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoItemTests
+    {
+        [Fact(DisplayName = "Novo Item Pedido com Unidades Abaixo do permitido")]
+        [Trait("Categoria", "Item Pedido Tests")]
+        //          Nome do Método      Estado                Resultado Esperado 
+        public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
+
+        }
+    }
+}
